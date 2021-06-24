be279b9ac9e122f0b6890a4381f4bbbb3a92939d
OmegaRogue <omegarogue@omegavoid.codes>
2021-06-24 14:30:56 +0200

    :page_facing_up: Correct Usage of GNU LGPL 3.0
    Correct Usage of LGPL to match fsf Recommendations
    
    Signed-off-by: OmegaRogue <omegarogue@omegavoid.codes>

--- journal/Cargo.go
+++ journal/Cargo.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2021 OmegaRogue.
+ *  This file is part of eliteJournal.
+ *
+ *     eliteJournal is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *     eliteJournal is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with eliteJournal.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package journal
 
 // Cargo Cargo
